2b04a09fcb2c70bfd862c6f555262fdab66750fd
Lorenzo Giuliani <lorenzo@frenzart.com>
2025-02-07 19:49:50 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "rimworld mods";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flakelight.url = "github:nix-community/flakelight";
   };
 
